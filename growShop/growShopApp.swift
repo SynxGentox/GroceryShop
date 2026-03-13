c6c1b7c7618467fbf7cb081d0adab2d8d49e3361
Aryan Verma <varyan069@gmail.com>
2026-03-13 10:44:32 +0530

    Initial Commit

---
+++ growShop/growShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  growShopApp.swift
+//  growShop
+//
+//  Created by Aryan Verma on 13/03/26.
+//
+
+import SwiftUI
+
+@main
+struct growShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
